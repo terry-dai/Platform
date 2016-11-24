c8ca7a804141131f9ec88413b3b64fc77c459af6
David Christ <david.christ@rea-group.com>
2016-11-24 13:44:00 +1100

    Finish address decoding

--- api/geocoding/index.js
+++ api/geocoding/index.js
@@ -1,34 +1,64 @@
-const NodeGeocoder = require('node-geocoder');
+'use strict';
 
+const NodeGeocoder = require('node-geocoder');
 const options = {
   provider: 'openstreetmap'
 };
-
 const geocoder = NodeGeocoder(options);
 
-geocoder.geocode('511 Church St, VIC 3121')
-  .then(function(res) {
-    console.log('geocode lazy');
-    console.log(res);
-    console.log('-------');
-  })
-  .catch(function(err) {
-    console.log(err);
-  });
 
-geocoder.geocode({
-    state: 'Victoria',
-    postalcode: '3121',
-    street: '511 Church Street',
-  })
-  .then(function(res) {
-    console.log('geocode object');
-    console.log(res);
-    console.log('-------');
-  })
-  .catch(function(err) {
-    console.log(err);
-  });
+const inputs = [
+  '511+Church+St%2C+Richmond%2C+VIC+3121',
+  '1%2F511+Church+St%2C+Richmond%2C+VIC+3121'
+];
+
+const cleanAddress = input => {
+  let address = input;
+
+  address = decodeURIComponent(address);
+
+  // Clean "+" instead of space usage
+  address = address.replace(/\+/g, ' ');
+
+  // Remove house number
+  if (address.match(/\d+\/\d+/)) {
+    address = address.split('/')[1];
+  }
+
+  // Remove suburb, which confuses OSM
+  address = address.split(',');
+  address = address[0] + ',' + address[2];
+
+  return address;
+};
+
+inputs.forEach(input => {
+  const address = cleanAddress(input);
+
+  geocoder.geocode(address)
+    .then(function(res) {
+      console.log('geocode: ' + address);
+      console.log(res);
+      console.log('-------');
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+});
+
+// geocoder.geocode({
+//     state: 'Victoria',
+//     postalcode: '3121',
+//     street: '511 Church Street',
+//   })
+//   .then(function(res) {
+//     console.log('geocode object');
+//     console.log(res);
+//     console.log('-------');
+//   })
+//   .catch(function(err) {
+//     console.log(err);
+//   });
 
 // geocoder.reverse({lat:-37.8288688, lon:144.9973862})
 //   .then(function(res) {
